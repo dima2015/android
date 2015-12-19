2a875162f66d7aa216732dc8d9ab9a2f6bf9d6ed
Claudio Cardinale <cardinale.claudio@gmail.com>
2015-12-19 01:13:43 +0100

    refactoring

--- app/src/main/java/com/plunner/plunner/models/Model.java
+++ app/src/main/java/com/plunner/plunner/models/Model.java
@@ -21,18 +21,15 @@ abstract class Model {
 
     static public String AUTH_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtb2RlIjoiZW4iLCJzdWIiOiIzNCIsImlzcyI6Imh0dHA6XC9cL2FwaS5wbHVubmVyLmNvbVwvZW1wbG95ZWVzXC9ncm91cHMiLCJpYXQiOiIxNDUwNDgwNjA2IiwiZXhwIjoiMTQ1MDQ4NzIxNCIsIm5iZiI6IjE0NTA0ODM2MTQiLCJqdGkiOiIyZjgwMzU3MzU5MjAzMmM4YjIyODIzMWJmM2U0Nzc1ZSJ9.-FHEGifidaZ0EvY-W5DWGImcoWNKjPE_SHcI1I6W0YU";
 
+    /**
+     * @param interfaceClass
+     * @param <T>
+     * @return
+     */
     static protected<T> T createRetrofit(Class<T> interfaceClass) {
-        Interceptor interceptor = new Interceptor() {
-            @Override
-            public Response intercept(Chain chain) throws IOException {
-                Request newRequest = chain.request().newBuilder().addHeader("Authorization", AUTH_TOKEN).build();
-                return chain.proceed(newRequest);
-            }
-        };
-
         // Add the interceptor to OkHttpClient
         OkHttpClient client = new OkHttpClient();
-        client.interceptors().add(interceptor);
+        client.interceptors().add(new InterceptorClass());
 
         return new Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -44,5 +41,14 @@ abstract class Model {
     }
 
     abstract public void fresh();
+
     abstract public void save();
+
+    static private class InterceptorClass implements Interceptor {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request newRequest = chain.request().newBuilder().addHeader("Authorization", AUTH_TOKEN).build();
+            return chain.proceed(newRequest);
+        }
+    }
 }
