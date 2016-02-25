461c088c03e4f59e3e3155d992e74f7e3f3f31cb
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-02-25 02:17:27 +0100

    planners/timeslots

---
+++ app/src/main/java/com/plunner/plunner/models/models/FatherParameters.java
@@ -0,0 +1,8 @@
+package com.plunner.plunner.models.models;
+
+/**
+ * Created by claudio on 25/02/16.
+ */
+public interface FatherParameters {
+    public void setFatherParameters(String... parameters);
+}
