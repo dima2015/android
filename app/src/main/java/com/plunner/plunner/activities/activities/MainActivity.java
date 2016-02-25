181573d6e111a4c93238e360646deea113ecd2fe
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-02-25 15:38:21 +0100

    draft network problem detect

--- app/src/main/java/com/plunner/plunner/activities/activities/MainActivity.java
+++ app/src/main/java/com/plunner/plunner/activities/activities/MainActivity.java
@@ -1,6 +1,10 @@
 package com.plunner.plunner.activities.activities;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.facebook.stetho.Stetho;
 import com.plunner.plunner.R;
@@ -24,10 +29,10 @@ import com.plunner.plunner.models.models.employee.Group;
 
 
 public class MainActivity extends AppCompatActivity {
-/**
- * @author Claudio Cardinale <cardi@thecsea.it>
- * @version 1.0.0
- */
+    /**
+     * @author Claudio Cardinale <cardi@thecsea.it>
+     * @version 1.0.0
+     */
     Employee employee = null;
     LoginManager loginManager;
 
@@ -106,6 +111,25 @@ public class MainActivity extends AppCompatActivity {
         //TODO automatically try to get token by long token
     }
 
+    public void onNoHttpError(NoHttpException e) {
+
+        ConnectivityManager cm =
+                (ConnectivityManager) MainActivity.this.getSystemService(MainActivity.this.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        if (!isConnected) {
+            //Run it on the main thread
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MainActivity.this, "Network problem", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
     private class EmployeeCallback implements
             CallOnHttpError<Employee>, CallOnNext<Employee>, CallOnNoHttpError<Employee> {
 
@@ -118,11 +142,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onNext(Employee employee) {
             MainActivity.this.employee = employee;
+            MainActivity.this.onNoHttpError(null);
         }
 
         @Override
         public void onNoHttpError(NoHttpException e) {
-            //TODO manage
+            MainActivity.this.onNoHttpError(e);
         }
     }
 
@@ -141,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onNoHttpError(NoHttpException e) {
-            //TODO manage
+            MainActivity.this.onNoHttpError(e);
         }
     }
 }
